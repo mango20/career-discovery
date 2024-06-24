2d1d073c818b652b1b393b3a39920c74a7d8efc0
Charmaine Lilan Manga <charmainelilanmanga@gmail.com>
2024-06-24 15:08:40 +0800

    [build]

--- server/src/app.js
+++ server/src/app.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5000;
 //express config
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 // app.use(setResponseHeadersMW);
 app.use(cors());
 app.use(apiLoggerMW);
