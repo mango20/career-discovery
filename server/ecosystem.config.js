e554e373fba3cbaa54e07707fd8b011aa83f744f
Charmaine Lilan Manga <charmainelilanmanga@gmail.com>
2024-06-25 19:35:19 +0800

    [edit] -scroll

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,20 +1,26 @@
 module.exports = {
   apps: [
     {
-      name: "server",
+      name: "sds",
       script: "src/app.js",
       watch: true,
+      ignore_watch: ["node_modules", "client"],
+      watch_options: {
+        followSymlinks: false,
+      },
       env: {
-        NODE_ENV: "development",
+        NODE_ENV: "production",
       },
     },
     {
-      name: "client",
+      name: "client-build",
       script: "npm",
-      args: "start --prefix client",
-      watch: true,
+      args: "run client:build",
+      cwd: "../client",
+      autorestart: false,
+      watch: false,
       env: {
-        NODE_ENV: "development",
+        NODE_ENV: "production",
       },
     },
   ],
