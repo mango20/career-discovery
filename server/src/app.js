801c56cb694e6f42f16bd4c6636ecd266f9f523f
Charmaine Lilan Manga <charmainelilanmanga@gmail.com>
2024-06-30 09:33:41 +0800

    [mid]

--- server/src/app.js
+++ server/src/app.js
@@ -28,7 +28,7 @@ const PORT = 8800;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet({ contentSecurityPolicy: false }));
-// app.use(setResponseHeadersMW);
+app.use(setResponseHeadersMW);
 app.use(cors());
 app.use(apiLoggerMW);
 
